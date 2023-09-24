d11d89304d6e9a8ce8181cd0a2304fe58b127ffd
know <do.i.know.it@yggdrashill.com>
2023-09-24 13:46:38 +0900

    Modify definition of life cycle order

--- Assets/YggdrAshill.Ragnarok.Unity/Runtime/Lifecycle/LifecycleExecutionOrder.cs
+++ Assets/YggdrAshill.Ragnarok.Unity/Runtime/Lifecycle/LifecycleExecutionOrder.cs
@@ -1,11 +1,9 @@
 #nullable enable
-using System;
-
 namespace YggdrAshill.Ragnarok
 {
     internal static class LifecycleExecutionOrder
     {
-        public const int Project = Int32.MinValue;
+        public const int Project = int.MinValue;
         public const int Scene = Project + 1;
         public const int GameObject = Scene + 2;
     }
