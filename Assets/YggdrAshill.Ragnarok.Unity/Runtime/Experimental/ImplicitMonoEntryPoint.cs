092020442cda424080f51b7e676e42380f724a8b
know <do.i.know.it@yggdrashill.com>
2023-09-24 19:08:48 +0900

    Add specification of ImplicitMonoEntryPoint

--- Assets/YggdrAshill.Ragnarok.Unity/Runtime/Experimental/ImplicitMonoEntryPoint.cs
+++ Assets/YggdrAshill.Ragnarok.Unity/Runtime/Experimental/ImplicitMonoEntryPoint.cs
@@ -8,13 +8,51 @@ namespace YggdrAshill.Ragnarok.Experimental
     public sealed class ImplicitMonoEntryPoint : MonoEntryPoint
     {
         protected override IEnumerable<IInstallation> InstallationList => GetInstallationList(transform);
-
-        private IEnumerable<IInstallation> GetInstallationList(Transform target)
+        private static IEnumerable<IInstallation> GetInstallationList(Transform target)
         {
-            return Enumerable.Range(0, target.childCount)
-                .Select(target.GetChild)
-                .Where(child => !child.TryGetComponent<Lifecycle>(out _) || !child.TryGetComponent<ImplicitMonoEntryPoint>(out _))
-                .SelectMany(GetInstallationList);
+            var current = GetCurrentInstallationList(target);
+
+            var children = GetChildInstallationList(target);
+
+            var grandChildren = GetGrandChildInstallationList(target);
+
+            return current.Concat(children).Concat(grandChildren);
+        }
+        private static IEnumerable<IInstallation> GetCurrentInstallationList(Transform target)
+        {
+            return target.GetComponents<IInstallation>().Where(installation =>
+            {
+                var implicitMonoEntryPoint = installation as ImplicitMonoEntryPoint;
+                var explicitMonoEntryPoint = installation as ExplicitMonoEntryPoint;
+
+                return implicitMonoEntryPoint == null && explicitMonoEntryPoint == null;
+            });
+        }
+        private static IEnumerable<IInstallation> GetChildInstallationList(Transform target)
+        {
+            return GetChildCandidate(target).SelectMany(GetCurrentInstallationList);
+        }
+        private static IEnumerable<IInstallation> GetGrandChildInstallationList(Transform target)
+        {
+            return GetChildCandidate(target).SelectMany(GetChildInstallationList);
+        }
+
+        private static IEnumerable<Transform> GetChildCandidate(Transform target)
+        {
+            return Enumerable.Range(0, target.childCount).Select(target.GetChild).Where(child =>
+            {
+                if (child.TryGetComponent<Lifecycle>(out _))
+                {
+                    return false;
+                }
+
+                if (child.TryGetComponent<ImplicitMonoEntryPoint>(out _))
+                {
+                    return false;
+                }
+
+                return !child.TryGetComponent<ExplicitMonoEntryPoint>(out _);
+            });
         }
     }
 }
---
+++ Assets/YggdrAshill.Ragnarok.Unity/Specification/Experimental/ImplicitMonoEntryPointSpecification.cs
@@ -0,0 +1,52 @@
+#nullable enable
+using YggdrAshill.Ragnarok.Experimental;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YggdrAshill.Ragnarok.Unity.Specification
+{
+    public class ImplicitMonoEntryPointSpecification : MonoInstallation
+    {
+        private sealed class HelloWorld
+        {
+            private readonly GameObject target;
+
+            [Inject]
+            public HelloWorld(GameObject target)
+            {
+                this.target = target;
+            }
+            
+            public void Execute()
+            {
+                Debug.LogError($"Hello world in {target.name}.");
+            }
+        }
+        
+        [SerializeField] private GameObject? target;
+        private GameObject Target
+        {
+            get
+            {
+                if (target == null)
+                {
+                    target = transform.gameObject;
+                }
+
+                return target;
+            }
+        }
+        
+        public override void Install(IContainer container)
+        {
+            container.Register<HelloWorld>(Lifetime.Temporal).WithArgument(nameof(target), Target);
+            container.Register(resolver =>
+            {
+                foreach (var helloworld in resolver.Resolve<IEnumerable<HelloWorld>>())
+                {
+                    helloworld.Execute();
+                }
+            });
+        }
+    }
+}
