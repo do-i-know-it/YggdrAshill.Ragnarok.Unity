9fc56f25ceadb8a4d7d68e7dab5bd8a135f3b8d8
know <do.i.know.it@yggdrashill.com>
2023-10-05 19:49:26 +0900

    Add specification for RegisterComponent

---
+++ Assets/YggdrAshill.Ragnarok.Unity/Specification/Scripts/ServiceComponent.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+using UnityEngine;
+
+namespace YggdrAshill.Ragnarok.Specification
+{
+    internal sealed class ServiceComponent : MonoBehaviour
+    {
+        [InjectField] private SenderComponent? sender;
+        [InjectField] private ReceiverComponent? receiver;
+
+        private void Awake()
+        {
+            var content = sender!.Send();
+            receiver!.Receive(content);
+        }
+    }
+}
