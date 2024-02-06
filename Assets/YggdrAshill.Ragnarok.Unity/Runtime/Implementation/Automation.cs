e6a9998ee51e787856685bcb90cf838f61f15cfb
know <do.i.know.it@yggdrashill.com>
2024-02-06 21:57:12 +0900

    Revert "Revert "Add implementations to register dependencies""
    This reverts commit 8ad478dc7bfaab5071221a765f5a1bf78058b7c3.

---
+++ Assets/YggdrAshill.Ragnarok.Unity/Runtime/Implementation/Automation.cs
@@ -0,0 +1,59 @@
+﻿#nullable enable
+using System;
+using UnityEngine;
+
+namespace YggdrAshill.Ragnarok
+{
+    [Serializable]
+    internal sealed class Automation
+    {
+        public static void Register(IInstanceInjection instanceInjection, InstanceInjectionTarget target, TypeAssignmentMethod method)
+        {
+            if ((target & InstanceInjectionTarget.Field) is InstanceInjectionTarget.Field)
+            {
+                instanceInjection.WithFieldInjection();
+            }
+            if ((target & InstanceInjectionTarget.Property) is InstanceInjectionTarget.Property)
+            {
+                instanceInjection.WithPropertyInjection();
+            }
+            if ((target & InstanceInjectionTarget.Method) is InstanceInjectionTarget.Method)
+            {
+                instanceInjection.WithMethodInjection();
+            }
+            
+            switch (method)
+            {
+                case TypeAssignmentMethod.Self:
+                    instanceInjection.AsOwnSelf();
+                    break;
+                case TypeAssignmentMethod.AllInterfaces:
+                    instanceInjection.AsImplementedInterfaces();
+                    break;
+                case TypeAssignmentMethod.AllInterfacesAndSelf:
+                    instanceInjection.AsImplementedInterfaces().AsOwnSelf();
+                    break;
+            }
+        }
+        
+        [SerializeField] private Component? component;
+        public Component Component
+        {
+            get
+            {
+                if (component == null)
+                {
+                    throw new InvalidOperationException($"{nameof(component)} is null.");
+                }
+
+                return component;
+            }
+        }
+
+        [SerializeField] private TypeAssignmentMethod typeAssignmentMethod = TypeAssignmentMethod.Self;
+        public TypeAssignmentMethod TypeAssignmentMethod => typeAssignmentMethod;
+        
+        [SerializeField] private InstanceInjectionTarget instanceInjectionTarget = InstanceInjectionTarget.None;
+        public InstanceInjectionTarget InstanceInjectionTarget => instanceInjectionTarget;
+    }
+}
---
+++ Assets/YggdrAshill.Ragnarok.Unity/Runtime/Implementation/InstanceInjectionTarget.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace YggdrAshill.Ragnarok
+{
+    [Flags]
+    public enum InstanceInjectionTarget : byte
+    {
+        None = 0,
+        Field = 1 << 0,
+        Property = 1 << 1,
+        Method = 1 << 2,
+    }
+}
